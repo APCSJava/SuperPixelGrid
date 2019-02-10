3db4be890aa295706669c57f530be22ddce5b337
Kent Collins <kent.r.collins@gmail.com>
2019-02-10 11:55:23 +0000

    inverted Y mouse coords

--- Colorizer.java
+++ Colorizer.java
@@ -10,8 +10,7 @@ public class Colorizer {
 	 * 
 	 * Postcondition -- all superpixels in the original remain unchanged
 	 * 
-	 * @param original
-	 *            a superpixel array that should not be altered
+	 * @param original a superpixel array that should not be altered
 	 * @return a new array with the properties described
 	 */
 	SuperPixel[][] commandWhite(SuperPixel[][] original) {
@@ -21,8 +20,7 @@ public class Colorizer {
 				if (Math.random() > 0.5) {
 					mod[r][c] = new SuperPixel(java.awt.Color.WHITE);
 				} else {
-					mod[r][c] = new SuperPixel(
-							original[r][c].getColor());
+					mod[r][c] = new SuperPixel(original[r][c].getColor());
 				}
 			}
 		}
@@ -35,8 +33,7 @@ public class Colorizer {
 	 * 
 	 * Postcondition -- all superpixels in the original remain unchanged
 	 * 
-	 * @param original
-	 *            a superpixel array that should not be altered
+	 * @param original a superpixel array that should not be altered
 	 * @return a new array with the properties described
 	 */
 	public SuperPixel[][] commandRed(SuperPixel[][] original) {
@@ -55,13 +52,17 @@ public class Colorizer {
 	 * 
 	 * Postcondition -- all superpixels in the original remain unchanged
 	 * 
-	 * @param original
-	 *            a superpixel array that should not be altered
+	 * @param original a superpixel array that should not be altered
 	 * @return a new array with the properties described
 	 */
 	SuperPixel[][] commandGreen(SuperPixel[][] original) {
-		// TODO implement this method
-		return null;
+		SuperPixel[][] mod = new SuperPixel[original.length][original[0].length];
+		for (int r = 0; r < mod.length; r++) {
+			for (int c = 0; c < mod[r].length; c++) {
+				mod[r][c] = c % 2 == 0 ? new SuperPixel(java.awt.Color.GREEN) : original[r][c];
+			}
+		}
+		return mod;
 	}
 
 	/**
@@ -71,24 +72,31 @@ public class Colorizer {
 	 * 
 	 * Postcondition -- all superpixels in the original remain unchanged
 	 * 
-	 * @param original
-	 *            a superpixel array that should not be altered
+	 * @param original a superpixel array that should not be altered
 	 * @return a new array with the properties described
 	 */
 	SuperPixel[][] commandBlue(SuperPixel[][] original) {
-		// TODO implement this method
-		return null;
+		SuperPixel[][] mod = new SuperPixel[original.length][original[0].length];
+		for (int r = 0; r < mod.length; r++) {
+			for (int c = 0; c < mod[r].length; c++) {
+				mod[r][c] = r % 3 == 0 ? new SuperPixel(java.awt.Color.BLUE) : original[r][c];
+			}
+		}
+		return mod;
 	}
 
 	/**
 	 * Modifies the original array by setting the color of all elements to black
 	 * (java.awt.Color.BLACK).
 	 * 
-	 * @param original
-	 *            a superpixel array to mutate
+	 * @param original a superpixel array to mutate
 	 */
 	void commandClear(SuperPixel[][] original) {
-		// TODO implement this method
+		for (int r = 0; r < original.length; r++) {
+			for (int c = 0; c < original[r].length; c++) {
+				original[r][c] = new SuperPixel(java.awt.Color.BLACK);
+			}
+		}
 	}
 
 	/**
@@ -96,19 +104,22 @@ public class Colorizer {
 	 * replaced with a row of all black pixels (ie. edges do not wrap when scrolling
 	 * up).
 	 * 
-	 * @param original
-	 *            a superpixel array to mutate
+	 * @param original a superpixel array to mutate
 	 */
 	void commandUp(SuperPixel[][] original) {
-		// TODO implement this method
+		for (int r = 0; r < original.length - 1; r++) {
+			original[r] = original[r+1];
+		}
+		for (int c = 0; c < original[0].length; c++) {
+			original[original.length - 1][c] = new SuperPixel(java.awt.Color.BLACK);
+		}
 	}
 
 	/**
 	 * Shifts all super pixels down one row. As the bottom row moves down, its
 	 * pixels reappear at the top (ie. edges wrap when scrolling down).
 	 * 
-	 * @param original
-	 *            a superpixel array to mutate
+	 * @param original a superpixel array to mutate
 	 */
 	void commandDown(SuperPixel[][] original) {
 		// TODO implement this method
@@ -119,15 +130,13 @@ public class Colorizer {
 	 * the screen they are replaced with black super pixels in the rightmost column
 	 * (ie. edges do not wrap when scrolling left)
 	 * 
-	 * @param original
-	 *            a superpixel array to mutate
+	 * @param original a superpixel array to mutate
 	 */
 	void commandLeft(SuperPixel[][] original) {
 		for (int r = 0; r < original.length; r++) {
 			for (int c = 0; c < original[0].length; c++) {
 				if (c == original[0].length - 1) {
-					original[r][c] = new SuperPixel(
-							java.awt.Color.BLACK);
+					original[r][c] = new SuperPixel(java.awt.Color.BLACK);
 				} else {
 					original[r][c] = original[r][c + 1];
 				}
@@ -140,8 +149,7 @@ public class Colorizer {
 	 * the screen they reappear at the left edge (ie. edges wrap when scrolling
 	 * right)
 	 * 
-	 * @param original
-	 *            a superpixel array to mutate
+	 * @param original a superpixel array to mutate
 	 */
 	void commandRight(SuperPixel[][] original) {
 		// TODO implement this method
@@ -150,8 +158,7 @@ public class Colorizer {
 	/**
 	 * Sets the color property of the parameter to white.
 	 * 
-	 * @param superPixel
-	 *            a single superpixel to mutate
+	 * @param superPixel a single superpixel to mutate
 	 */
 	public static void modifySinglePixel(SuperPixel superPixel) {
 		superPixel.setColor(java.awt.Color.WHITE);
@@ -162,8 +169,7 @@ public class Colorizer {
 	 * Life. Examine each cell in the current generation and determine its state in
 	 * the subsequent generation based on the state of surrounding cells.
 	 * 
-	 * @param original
-	 *            a superpixel array to mutate
+	 * @param original a superpixel array to mutate
 	 */
 	public void lifeCommand(SuperPixel[][] original) {
 		// TODO implement this method
--- SuperPixelGrid.java
+++ SuperPixelGrid.java
@@ -12,13 +12,14 @@ public class SuperPixelGrid {
 
 	private final int SCREEN_WIDTH = 512;
 	private final int SCREEN_HEIGHT = 512;
+	private final int CELL_SIZE = 16;
 
 	private Colorizer c;
 	private SuperPixel[][] pixels;
 	private SuperPixel[][] buffer;
 	private SuperPixel[][] previous;  // used for undo action
 	private boolean toolTips = false;
-	int[] mouseCoords = { -1, -1 }; // row and column
+	int[] cellCoords = { -1, -1 }; // row and column from upper left
 
 	public static void main(String args[]) {
 		SuperPixelGrid spg = new SuperPixelGrid();
@@ -26,7 +27,7 @@ public class SuperPixelGrid {
 	}
 
 	public SuperPixelGrid() {
-		pixels = new SuperPixel[32][32];
+		pixels = new SuperPixel[SCREEN_WIDTH/CELL_SIZE][SCREEN_HEIGHT/CELL_SIZE];
 		for (int r = 0; r < pixels.length; r++) {
 			for (int c = 0; c < pixels[r].length; c++) {
 				pixels[r][c] = new SuperPixel(Color.BLACK);
@@ -76,6 +77,7 @@ public class SuperPixelGrid {
 				buffer = pixels;
 				pixels = previous;
 				previous = buffer;
+				StdDraw.pause(100);
 			}
 		} else if (StdDraw.isKeyPressed(KeyEvent.VK_SPACE)) {
 			if (buffer != null) {
@@ -95,21 +97,13 @@ public class SuperPixelGrid {
 
 	private void checkMouse() {
 		double mouseX = StdDraw.mouseX();
-		double mouseY = StdDraw.mouseY();
-
-		// calculate which column is under mouse
-		if (mouseX >= 0 && mouseX < SCREEN_WIDTH) {
-			mouseCoords[1] = (int) (mouseX / 16.0);
-		} else
-			mouseCoords[1] = -1;
-		if (mouseY >= 0 && mouseY < SCREEN_HEIGHT) {
-			mouseCoords[0] = (int) (mouseY / 16.0);
-		} else
-			mouseCoords[0] = -1;
-
+		double mouseY = SCREEN_HEIGHT-StdDraw.mouseY();
+		int mouseRow = (int) (mouseY/CELL_SIZE);
+		int mouseCol = (int) (mouseX/CELL_SIZE);
+		cellCoords = new int[] {mouseRow, mouseCol};
 		// deal with mouse presses
 		if (StdDraw.isMousePressed()) {
-			SuperPixel sp = pixels[mouseCoords[0]][mouseCoords[1]];
+			SuperPixel sp = pixels[mouseRow][mouseCol];
 			Colorizer.modifySinglePixel(sp);
 			while (StdDraw.isMousePressed()) {
 				// wait for mouse release
@@ -123,14 +117,14 @@ public class SuperPixelGrid {
 			for (int c = 0; c < pixels[r].length; c++) {
 				SuperPixel sp = pixels[r][c];
 				StdDraw.setPenColor(sp.getColor());
-				StdDraw.filledCircle(c * 16 + 8, r * 16 + 8,
+				StdDraw.filledCircle(c * 16 + 8, SCREEN_HEIGHT- (r * 16) - 8,
 						sp.getSize());
 			}
 		}
 		if (toolTips) {
 			StdDraw.setPenColor(java.awt.Color.WHITE);
-			int row = 31 - mouseCoords[0]; // display upper left corner as (0, 0)
-			int col = mouseCoords[1];
+			int row = cellCoords[0]; // display upper left corner as (0, 0)
+			int col = cellCoords[1];
 			StdDraw.text(StdDraw.mouseX(), StdDraw.mouseY(),
 					"(" + row + "," + col + ")");
 		}
